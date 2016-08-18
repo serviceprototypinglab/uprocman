c7a24ce0de8c8b37c190a592cd6008a2e46c3e09
spio <spio@tiguriner>
2016-08-18 17:42:03 +0200

    - several fixes from testing with roslaunch, adapted to working also (still) on other systems

--- tracer/procman-tracetest.sh
+++ tracer/procman-tracetest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # strace output:
-# strace: Process 16776 attached
+# strace: Process 16776 attached (strace 4.12/Debian)
+# Process 16776 attached (strace 4.8/Ubuntu)
 # [pid 16776] +++ killed by SIGTERM +++
 # [pid 18266] +++ exited with 0 +++
 # +++ exited with 0 +++ {for last remaining pid}
@@ -32,13 +33,20 @@ else
 	colr=""
 fi
 
-strace -e trace=fork -f $* 2>&1 | \
+# Note: application output is filtered away as it may interfere with strace output
+# (e.g. Process xxx attached may be shifted over a line output by roslaunch)
+strace -e trace=fork -f $* 2>&1 >/dev/null | \
 while read line
 do
 	echo $line | grep -q "attached"
 	if [ $? = 0 ]
 	then
 		pid=`echo $line | cut -d " " -f 3`
+		if [ $pid = "attached" ]
+		then
+			# Note: strace 4.8 syntax
+			pid=`echo $line | cut -d " " -f 2`
+		fi
 		echo -e "${cola}+$pid$colr" >> $out
 	fi
 	echo $line | grep -q "killed"
